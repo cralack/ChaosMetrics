9f0efd29025be0b9c86b342f70dabe0b52232b70
linkman <cralack@hotmail.com>
2024-02-05 20:39:25 +0800

    chore(project): initialize frontend project

--- web/src/main.js
+++ web/src/main.js
@@ -1,17 +1,5 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
 
-const app = createApp(App)
-
-// 整合element-plus
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-// 全局引入字体图标
-import * as ElIcons from '@element-plus/icons-vue'
-for (const name in ElIcons) {
-    app.component(name, (ElIcons)[name])
-}
-app.use(ElementPlus)
-
-
-app.mount('#app')
+createApp(App).mount('#app')
